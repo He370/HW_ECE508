782383bd136c113476c380fb9c90925b13f5102e
Lindun He <ldh.sjtu@gmail.com>
2018-02-06 18:17:38 -0500

    update s.sh

--- HW1/s.sh
+++ HW1/s.sh
@@ -2,4 +2,4 @@
 
 javac Switch.java
 jar cvfm Switch.jar manifest_switch.txt Switch.class SwitchSockHandler.class SwitchTimerTask.class
-java -cp Switch.jar Switch 1 2000
+java -cp Switch.jar Switch $1 $2
