7b3f528336a624a0269b6f0179ac0769b398a303
alarea1 <mailuanlai@pal-nat186-94-233.itap.purdue.edu>
2018-02-06 15:46:15 -0500

    first Vision

---
+++ HW1/Read.java
@@ -0,0 +1,128 @@
+package ReadFile;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.text.AbstractDocument.Content;
+
+import Dijkstra.DijkstraAlgorithm;
+import Dijkstra.Edge;
+import Dijkstra.Graph;
+import Dijkstra.Vertex;
+
+public class Read {
+	List<Vertex> nodes;
+	List<Edge> edges;
+	public Read(List<Vertex> nodes, List<Edge> edges) {
+		this.nodes = nodes;
+		this.edges = edges;
+	}
+	public static int top = 0;
+	public Hashtable<String, HashSet<String>> getNeighbors() throws IOException{
+		Hashtable<String, HashSet<String>> neighbors = new Hashtable<>();
+		ArrayList<String> answer = new ArrayList<>();
+		String filePath = "src/txt/test.txt";
+		 File file = new File(filePath);
+		 FileInputStream inputStream = new FileInputStream(filePath);  
+	     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));  
+	     String str = null;  
+	        while((str = bufferedReader.readLine()) != null)  
+	        {  
+	        	answer.add(str);
+	        }
+	        for(int r = 0; r < answer.size(); r++) {
+	        	String[] row = answer.get(r).split(" ");
+	        	int start = Integer.valueOf(row[0]) ;
+	        	int end = Integer.valueOf(row[1]);
+	        	if(start >= top) top = start;
+	        	if(end >= top) top = end;	
+	        }
+	        for(int i = 0; i <= top; i++) {
+	        	neighbors.put(String.valueOf(i), new HashSet<String>());
+	        }
+	        for(int r = 0; r < answer.size(); r++) {
+	        	String[] row = answer.get(r).split(" ");
+	        	String start = (row[0]) ;
+	        	String end = (row[1]);
+	        	neighbors.get(start).add(end);
+	        	neighbors.get(end).add(start);
+	        }
+	        return neighbors;
+	} 
+	public ArrayList<String> read() throws IOException {
+		ArrayList<String> routing_tab = new ArrayList<>();
+		ArrayList<String> answer = new ArrayList<>();
+		String filePath = "src/txt/test.txt";
+		 File file = new File(filePath);
+		 FileInputStream inputStream = new FileInputStream(filePath);  
+	     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));  
+	     String str = null;  
+	        while((str = bufferedReader.readLine()) != null)  
+	        {  
+	        	answer.add(str);
+	        }  
+	        for(int r = 0; r < answer.size(); r++) {
+	        	String[] row = answer.get(r).split(" ");
+	        	int start = Integer.valueOf(row[0]) ;
+	        	int end = Integer.valueOf(row[1]);
+	        	if(start >= top) top = start;
+	        	if(end >= top) top = end;
+	        	
+	        }
+
+	    for(int i = 0; i <= top; i++) {
+	        	Vertex location = new Vertex(String.valueOf(i), "Node_" + i);
+	            nodes.add(location);
+	    }
+        for(int r = 0; r < answer.size(); r++) {
+        	String[] row = answer.get(r).split(" ");
+        	addLane(Integer.valueOf(row[0]), Integer.valueOf(row[1]), Integer.valueOf(row[2]));
+        	addLane(Integer.valueOf(row[1]), Integer.valueOf(row[0]), Integer.valueOf(row[2]));
+        }
+        
+        Graph graph = new Graph(nodes, edges);
+        for(int i = 0; i <= top ; i++) {
+        	DijkstraAlgorithm dijkstra = new DijkstraAlgorithm(graph);
+        	dijkstra.execute(nodes.get(i));
+        	routing_tab.add("");
+        	for(int j = 0; j <= top; j++) {
+       	        LinkedList<Vertex> path = dijkstra.getPath(nodes.get(j));
+       	        if(path == null) {
+       	        	String message = routing_tab.get(i)  + "-1"+ " ";
+       	        	routing_tab.set(i, message);
+       	        	System.out.println("the path from source " + i +" to destination " + j + "is: ");
+       	        	System.out.println("null");
+       	        	
+       	        }
+       	        else if(path.size() > 0){
+       	        	String message = routing_tab.get(i)  + path.get(1).getId()+ " ";
+       	        	routing_tab.set(i, message);
+       	        	System.out.println("the path from source " + i +" to destination " + j + "is: ");
+       	        	for (Vertex vertex : path) {
+		 	            System.out.print("Node: "+ vertex.getId());
+		 	            System.out.print(" ");   
+		 	        }
+       	        	System.out.println("");	
+       	        }
+        	}
+        }
+        return routing_tab;
+	}
+	
+	public void addLane(int sourceLocNo, int destLocNo,
+	            int duration) {
+	        Edge lane = new Edge(nodes.get(sourceLocNo), nodes.get(destLocNo), duration);
+	        edges.add(lane);
+	    }
+
+}
---
+++ HW1/udp_server.java
@@ -0,0 +1,108 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.logging.Handler;
+import org.junit.experimental.theories.Theories;
+import org.junit.internal.builders.AllDefaultPossibilitiesBuilder;
+
+import Dijkstra.Edge;
+import Dijkstra.Vertex;
+import ReadFile.Read;
+
+public class udp_server {
+	public static List<Vertex> nodes = new ArrayList<Vertex>();
+	public static List<Edge> edges = new ArrayList<Edge>();
+	public static Hashtable<String, HashSet<String>> neighbors;
+	public static String[] hostAddress;
+	public static String[] port;
+	public static String[] alive;
+	public static int count = 0;
+	public static void main(String args[]) throws IOException
+    {	
+		Read reader = new Read(nodes, edges);
+		neighbors = reader.getNeighbors();
+		hostAddress = new String[neighbors.size()];
+		for(int i = 0; i < neighbors.size(); i++) {
+			hostAddress[i] = "NULL";
+		}
+		port = new String[neighbors.size()];
+		for(int i = 0; i < neighbors.size(); i++) {
+			port[i] = "NULL";
+		}
+		alive = new String[neighbors.size()];
+		for(int i = 0; i < neighbors.size(); i++) {
+			alive[i] = "0";
+		}
+        DatagramSocket sock = null;
+         
+        try
+        {
+           
+            sock = new DatagramSocket(7777);
+            byte[] buffer = new byte[65536];
+            DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
+            echo("Server socket created. Waiting for incoming data...");
+             
+            
+            while(true)
+            {
+                sock.receive(incoming);
+                byte[] data = incoming.getData();
+                String s = new String(data, 0, incoming.getLength());
+                echo(incoming.getAddress().getHostAddress() + " : " + incoming.getPort() + " - " + s);
+                String[] message = s.split(" ");
+                String answer = "";
+                
+                if(message[0] == "0") {
+                	answer += "1" + " ";
+                	String id = message[1];
+                	hostAddress[Integer.valueOf(id) - 0] = incoming.getAddress().getHostAddress();
+                	port[Integer.valueOf(id) - 0] = String.valueOf(incoming.getPort());
+                	alive[Integer.valueOf(id) - 0] = "1";
+                	HashSet<String> neighbor = neighbors.get(id);
+                	for(String n: neighbor) {
+                		int n_id = Integer.valueOf(n);
+                		answer = answer + hostAddress[n_id] + " ";
+                		answer = answer + port[n_id] + " ";
+                		answer = answer + alive[n_id] + " ";
+                	}
+                	answer += "EOF";
+                	DatagramPacket dp = new DatagramPacket(answer.getBytes() , answer.getBytes().length , incoming.getAddress() , incoming.getPort());
+                    sock.send(dp);
+                }
+                String all = "1";
+                for(String a: alive) {
+                	if(a == "0") {
+                		all = "0";
+                	}
+                }
+                if(count == 0 && all == "1") {
+                	ArrayList<String> routing_tab = reader.read();
+                	for(int i = 0; i < routing_tab.size(); i++) {
+                		String ans = "3" + " ";
+                		ans += routing_tab.get(i);
+                		ans += "EOF";
+                		DatagramPacket dp = new DatagramPacket(ans.getBytes() , ans.getBytes().length , InetAddress.getByName(hostAddress[i]) , Integer.valueOf(port[i]));
+                        sock.send(dp);
+                	}
+                }
+            }
+        }
+         
+        catch(IOException e)
+        {
+            System.err.println("IOException " + e);
+        }
+    }
+     
+    //simple function to echo data to terminal
+    public static void echo(String msg)
+    {
+        System.out.println(msg);
+    }
+}
